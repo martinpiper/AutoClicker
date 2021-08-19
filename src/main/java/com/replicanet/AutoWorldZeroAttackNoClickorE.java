0cd788919c84ee4947684567fcfee6ad8a636095
Martin Piper <martin.piper@gmail.com>
2021-08-20 00:46:01 +0800

    Adding more automation

---
+++ src/main/java/com/replicanet/AutoWorldZeroAttackNoClickorE.java
@@ -0,0 +1,53 @@
+package com.replicanet;
+
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+public class AutoWorldZeroAttackNoClickorE {
+    public static void main(String args[]) {
+        try {
+            System.out.println("Will be clicking... ");
+            Thread.sleep(5000);
+            System.out.println("Will be clicking... ");
+            Robot robot = new Robot();
+            Point originalPoint = MouseInfo.getPointerInfo().getLocation();
+            while (true) {
+                Point testPoint = MouseInfo.getPointerInfo().getLocation();
+                if (Math.abs(testPoint.x - originalPoint.x) > 100) {
+                    break;
+                }
+
+                for (int i = 0; i < 5 ; i++) {
+                    robot.keyPress(KeyEvent.VK_R);
+                    Thread.sleep(50);
+                    robot.keyRelease(KeyEvent.VK_R);
+                    Thread.sleep(50);
+                }
+                for (int i = 0; i < 5 ; i++) {
+                    robot.keyPress(KeyEvent.VK_F);
+                    Thread.sleep(50);
+                    robot.keyRelease(KeyEvent.VK_F);
+                    Thread.sleep(50);
+                }
+                for (int i = 0; i < 5 ; i++) {
+                    robot.keyPress(KeyEvent.VK_1);
+                    Thread.sleep(50);
+                    robot.keyRelease(KeyEvent.VK_1);
+                    Thread.sleep(50);
+                }
+                for (int i = 0; i < 5 ; i++) {
+                    robot.keyPress(KeyEvent.VK_X);
+                    Thread.sleep(50);
+                    robot.keyRelease(KeyEvent.VK_X);
+                    Thread.sleep(50);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Stopped");
+    }
+}
