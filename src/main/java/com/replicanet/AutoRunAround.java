1f185e421963575aaead9ccd36ce93d1df382799
Martin Piper <martin.piper@gmail.com>
2021-11-25 16:29:31 +0800

    Slightly better autom run around

--- src/main/java/com/replicanet/AutoRunAround.java
+++ src/main/java/com/replicanet/AutoRunAround.java
@@ -3,6 +3,7 @@ package com.replicanet;
 import java.awt.*;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
+import java.util.Random;
 
 public class AutoRunAround {
     public static void main(String args[]) {
@@ -12,29 +13,42 @@ public class AutoRunAround {
             System.out.println("Will be clicking... ");
             Robot robot = new Robot();
             Point originalPoint = MouseInfo.getPointerInfo().getLocation();
+            Random random = new Random();
             while (true) {
                 Point testPoint = MouseInfo.getPointerInfo().getLocation();
                 if (Math.abs(testPoint.x - originalPoint.x) > 100) {
                     break;
                 }
 
-                robot.keyPress(KeyEvent.VK_W);
-                Thread.sleep(4000);
-                robot.keyRelease(KeyEvent.VK_W);
-                robot.keyPress(KeyEvent.VK_D);
-                Thread.sleep(3000);
-                robot.keyRelease(KeyEvent.VK_D);
-                robot.keyPress(KeyEvent.VK_S);
-                Thread.sleep(1000);
-                robot.keyRelease(KeyEvent.VK_S);
-                robot.keyPress(KeyEvent.VK_A);
-                Thread.sleep(2000);
-                robot.keyRelease(KeyEvent.VK_A);
-                robot.keyPress(KeyEvent.VK_SPACE);
-                Thread.sleep(2000);
-                robot.keyRelease(KeyEvent.VK_SPACE);
+                if (random.nextBoolean()) {
+                    robot.keyPress(KeyEvent.VK_W);
+                    Thread.sleep(4000);
+                    robot.keyRelease(KeyEvent.VK_W);
+                }
+                if (random.nextBoolean()) {
+                    robot.keyPress(KeyEvent.VK_D);
+                    Thread.sleep(3000);
+                    robot.keyRelease(KeyEvent.VK_D);
+                }
+                if (random.nextBoolean()) {
+                    robot.keyPress(KeyEvent.VK_S);
+                    Thread.sleep(1000);
+                    robot.keyRelease(KeyEvent.VK_S);
+                }
+                if (random.nextBoolean()) {
+                    robot.keyPress(KeyEvent.VK_A);
+                    Thread.sleep(2000);
+                    robot.keyRelease(KeyEvent.VK_A);
+                }
+                if (random.nextBoolean()) {
+                    robot.keyPress(KeyEvent.VK_SPACE);
+                    Thread.sleep(2000);
+                    robot.keyRelease(KeyEvent.VK_SPACE);
+                }
 
-                Thread.sleep(60000);
+                if (random.nextBoolean()) {
+                    Thread.sleep(10000);
+                }
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
